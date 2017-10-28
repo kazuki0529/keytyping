aec25a3a8abb4d9952d79861b28b0c3269741a13
kazuki0529 <luis.figo.xx.the.leopard@gmail.com>
2017-10-28 19:23:16 +0900

    pububのkey情報とチャンネル情報の分離

--- src/js/const/pubnub.js
+++ src/js/const/pubnub.js
@@ -3,10 +3,3 @@
  */
 const PUBLISH_KEY	= '******************************************';	//PUBNUBのpublish_key
 const SUBSCRIBE_KEY	= '******************************************';	//PUBNUBのsubscribe_key
-
-/**
- * PUBNUBのチャンネル定義
- */
-const GAME_PROGRESS	= 'GAME_PROGRESS';	// ゲーム進行用のチャンネル
-const ANSWER	    = 'ANSWER';	        // 回答（入力）者用のチャンネル
-const GAME_CONTROL	= 'GAME_CONTROL';	// ゲーム操作用のチャンネル
