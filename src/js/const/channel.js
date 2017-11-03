4f5e8d14db8ac754c1b4d825436890606e0b8d36
tetsuya.zama <tetsuya.zama@gmail.com>
2017-11-03 14:12:23 +0900

    quiz_master仮実装

--- src/js/const/channel.js
+++ src/js/const/channel.js
@@ -3,3 +3,7 @@
  */
 const ROUND_PROGRESS	= 'ROUND_PROGRESS';	// ラウンド進行用のチャンネル
 const ANSWER	    	= 'ANSWER';	        // 回答（入力）者用のチャンネル
+
+
+const QUIZ_PROGRESS = 'QUIZ_PROGRESS'; //クイズ進行用のチャンネル
+const QUIZ_ANSWER = 'QUIZ_ANSWER'; //クイズ回答用チャンネル
