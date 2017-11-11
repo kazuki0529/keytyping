db792e0cf592baf3ed9e171d4574d44bf5e3cfe0
kazuki0529 <luis.figo.xx.the.leopard@gmail.com>
2017-11-12 00:35:00 +0900

    ランキング表示に対応

--- src/js/const/team.js
+++ src/js/const/team.js
@@ -9,8 +9,8 @@ const TEAM = {
 
 // チームのロゴ情報
 const TEAM_LOGO = [
-	{ key: TEAM.SPRING, image: './img/spring.png', color: 'rgb(202, 235, 190)' },
-	{ key: TEAM.SUMMER, image: './img/summer.png', color: 'rgb(194, 215, 233)' },
-	{ key: TEAM.AUTUMN, image: './img/autumn.png', color: 'rgb(255, 225, 169)' },
-	{ key: TEAM.WINTER, image: './img/winter.png', color: 'rgb(243, 199, 199)' }
+	{ key: TEAM.SPRING, view:"春", image: './img/spring.png', color: 'rgb(202, 235, 190)' },
+	{ key: TEAM.SUMMER, view:"夏", image: './img/summer.png', color: 'rgb(194, 215, 233)' },
+	{ key: TEAM.AUTUMN, view:"秋", image: './img/autumn.png', color: 'rgb(255, 225, 169)' },
+	{ key: TEAM.WINTER, view:"冬", image: './img/winter.png', color: 'rgb(243, 199, 199)' }
 ];
